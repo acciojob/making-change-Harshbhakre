57b22324d099805be034e32343262ecb16e42e10
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-14 11:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const makeChange = (c) => {
-  // your name here
+    let q = 0 
+    let value = 0
+    while(c>value+25){
+        value += 25
+        q++;
+    }
+     let d = 0 
+    while(c>value+10){
+        value += 10
+        d++;
+    }
+     let n = 0 
+    while(c>value+5){
+        value += 5
+        n++;
+    }
+     let p = 0 
+    while(c>=value+1){
+        value += 1
+        p++;
+    };
+      let obj = {
+  q: q,
+  d: d,
+  n: n,
+  p: p
 };
+    return obj
+}
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
